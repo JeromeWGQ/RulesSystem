0e160c3db73776ce19830c1c219a6954d17dd6cc
王国桥 <wfdwgq2013@126.com>
2017-02-07 20:31:39 +0800

    commit - 0207

---
+++ Rules/src/cn/edu/bjtu/rules/dao/LoginDao.java
@@ -0,0 +1,7 @@
+package cn.edu.bjtu.rules.dao;
+
+/**
+ * Created by Jerome on 2017/2/7.
+ */
+public class LoginDao {
+}
---
+++ Rules/src/cn/edu/bjtu/rules/service/LoginService.java
@@ -0,0 +1,7 @@
+package cn.edu.bjtu.rules.service;
+
+/**
+ * Created by Jerome on 2017/2/7.
+ */
+public class LoginService {
+}
